e41df33bb7e754b06a933ab00637885359d718d3
Alexander Garcia <alexthekidmg@gmail.com>
2024-11-24 16:59:49 -0500

    reload fixes

--- main.cpp
+++ main.cpp
@@ -13,24 +13,30 @@
 using namespace std;
 #pragma comment(lib, "Ws2_32.lib")
 
-/*
-int ListProcesses(){
+class Memory{
+    public: 
+    string Name;
+    DWORD Address;
+    vector<DWORD> Offsets;
+    DWORD Pointer;
+    
 
-    HANDLE Snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    public: Memory(string Name, DWORD address, vector<DWORD> offsets, HANDLE ProcessHandle, DWORD BaseAddress){
+        this->Address = address;
+        this->Name = Name;
+        this->Offsets = offsets;
+        Pointer = GetPointerAddress(ProcessHandle, BaseAddress, Address, Offsets);
+    }
 
-    PROCESSENTRY32 Entry;
-    Entry.dwSize = sizeof(PROCESSENTRY32);
+    public: void SetPointer(HANDLE ProcessHandle, DWORD BaseAddress){
+        Pointer = GetPointerAddress(ProcessHandle, BaseAddress, Address, Offsets);
+    }
 
-        if(Process32First(Snapshot, &Entry) == TRUE){
-            while(Process32Next(Snapshot, &Entry) == TRUE){
-                cout << Entry.szExeFile << endl;
-            }
-        }
-        CloseHandle(Snapshot);
-    return 1;
-} 
-*/
+    public: void SetPointerRelative(DWORD RelAddr, int offset){
+        Pointer = RelAddr + offset;
+    }
 
+};
 
 DWORD GetProcessIdByName(string s){
     HANDLE Snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -51,7 +57,6 @@ DWORD GetProcessIdByName(string s){
         return NULL;
 }
 
-//NEED TO REMAKE THIS FUNCTION MYSELF TO UNDERSTAND IT
 DWORD GetPointerAddress(HANDLE phandle, DWORD gameBaseAddr, DWORD address, vector<DWORD> offsets)
 {
  
@@ -85,53 +90,8 @@ MODULEENTRY32 GetModuleFromProcess(DWORD pId){
     return entry;
 }
 
-class Memory{
-    public: 
-    string Name;
-    DWORD Address;
-    vector<DWORD> Offsets;
-    DWORD Pointer;
-    
-
-    public: Memory(string Name, DWORD address, vector<DWORD> offsets, HANDLE ProcessHandle, DWORD BaseAddress){
-        this->Address = address;
-        this->Name = Name;
-        this->Offsets = offsets;
-        Pointer = GetPointerAddress(ProcessHandle, BaseAddress, Address, Offsets);
-    }
-
-    public: void SetPointerRelative(DWORD RelAddr, int offset){
-        Pointer = RelAddr + offset;
-    }
-
-};
-
-atomic<bool> godmode = false;
-atomic<bool> infammo = false;
 
 
-void enableGodMode(HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer,SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten){
-    while(true){
-        if (godmode){
-            //cout << "godmode enabled" << endl;
-            int health = 1000;
-            WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, lpNumberOfBytesWritten);
-
-        }
-    }
-
-}
-
-void enableInfAmmo(HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer,SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten){
-    while(true){
-        if (infammo){
-            //cout << "godmode enabled" << endl;
-            int health = 1000;
-            WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, lpNumberOfBytesWritten);
-
-        }
-    }
-}
 
 
 void inputInteger(const string& message, int& destination){       
@@ -199,26 +159,59 @@ void printMenu(){
     cout << "q. Quit" << endl;
 }
 
+
+DWORD pId;
+HANDLE Process;
+MODULEENTRY32 Module;
+DWORD BaseAddress;
+
+Memory yPos("Y Value", 0x009E41BC, {0x48, 0x970, 0x14,0x14,0x14,0x10,0x74}, Process, BaseAddress);
+Memory xPos("X Value", 0x0, {0x0}, Process, BaseAddress);
+Memory zPos("Z Value", 0x0, {0x0}, Process, BaseAddress);
+Memory playerChar("Character", 0xD7C9C0, {0x5118}, Process, 0);
+Memory playerHealth("Health", 0xDE41BC, {0x14C, 0x13BC}, Process, 0);
+Memory playerInventory("Inventory", 0xD7C9C0, {0}, Process, 0);
+Memory ammo("Ammo", 0x0097C938, {0x168, 0x4, 0xFFC}, Process, BaseAddress);
+
+
+atomic<bool> godmode = false;
+atomic<bool> infammo = false;
+
+
+void enableGodMode(HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer,SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten){
+    while(true){
+        if (godmode){
+            //cout << "godmode enabled" << endl;
+            int health = 1000;
+            playerHealth.SetPointer(Process, BaseAddress);
+            WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, lpNumberOfBytesWritten);
+
+        } 
+    }
+
+}
+
+void enableInfAmmo(HANDLE hProcess, LPVOID lpBaseAddress, LPCVOID lpBuffer,SIZE_T nSize, SIZE_T* lpNumberOfBytesWritten){
+    while(true){
+        if (infammo){
+            //cout << "godmode enabled" << endl;
+            int health = 1000;
+            ammo.SetPointer(Process, BaseAddress);
+            WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, lpNumberOfBytesWritten);
+
+        }
+    }
+}
+
 int main()
 {
     
-    DWORD pId = GetProcessIdByName("bhd.exe");
-    HANDLE Process  = OpenProcess(PROCESS_ALL_ACCESS, 0, pId);
-    MODULEENTRY32 Module = GetModuleFromProcess(pId);
-    DWORD BaseAddress = (DWORD)Module.modBaseAddr;
-
+    pId = GetProcessIdByName("bhd.exe");
+    Process  = OpenProcess(PROCESS_ALL_ACCESS, 0, pId);
+    Module = GetModuleFromProcess(pId);
+    BaseAddress = (DWORD)Module.modBaseAddr;
     
     string input;
-
-    Memory yPos("Y Value", 0x009E41BC, {0x48, 0x970, 0x14,0x14,0x14,0x10,0x74}, Process, BaseAddress);
-    Memory xPos("X Value", 0x0, {0x0}, Process, BaseAddress);
-    Memory zPos("Z Value", 0x0, {0x0}, Process, BaseAddress);
-    xPos.SetPointerRelative(yPos.Pointer, -4);
-    zPos.SetPointerRelative(yPos.Pointer, 4);
-    Memory playerChar("Character", 0xD7C9C0, {0x5118}, Process, 0);
-    Memory playerHealth("Health", 0xDE41BC, {0x14C, 0x13BC}, Process, 0);
-    Memory playerInventory("Inventory", 0xD7C9C0, {0}, Process, 0);
-    Memory ammo("Ammo", 0x0097C938, {0x168, 0x4, 0xFFC}, Process, BaseAddress);
 
     int godhealth = 10000;
     int infAmmo = 10000;
@@ -237,12 +230,22 @@ int main()
 
         if (selection == "1"){
             godmode = !godmode;
+            if(!godmode){
+                playerHealth.SetPointer(Process, BaseAddress);
+                int maxhealth = ReadProcessMemory(Process, (LPVOID)(playerHealth.Pointer + 4), &godhealth, sizeof(godhealth), nullptr );
+                WriteProcessMemory(Process, (LPVOID)playerHealth.Pointer, &maxhealth, sizeof(maxhealth), nullptr );
+            }
         }
         else if (selection == "2"){
             float x,y,z;
             inputFloat("enter X position", x);
             inputFloat("enter Y position", y);
             inputFloat("enter Z position", z);
+
+            yPos.SetPointer(Process, BaseAddress);
+            xPos.SetPointerRelative(yPos.Pointer, -4);
+            zPos.SetPointerRelative(yPos.Pointer, 4);
+
             WriteProcessMemory(Process, (LPVOID)xPos.Pointer, &x, sizeof(x), nullptr);
             WriteProcessMemory(Process, (LPVOID)yPos.Pointer, &y, sizeof(y), nullptr);
             WriteProcessMemory(Process, (LPVOID)zPos.Pointer, &z, sizeof(z), nullptr);
@@ -250,6 +253,11 @@ int main()
         else if (selection == "3"){
             float x,y,z;
             float dx,dy,dz;
+
+            yPos.SetPointer(Process, BaseAddress);
+            xPos.SetPointerRelative(yPos.Pointer, -4);
+            zPos.SetPointerRelative(yPos.Pointer, 4);
+
             ReadProcessMemory(Process, (LPVOID)xPos.Pointer, &x, sizeof(x), nullptr);
             ReadProcessMemory(Process, (LPVOID)yPos.Pointer, &y, sizeof(y), nullptr);
             ReadProcessMemory(Process, (LPVOID)zPos.Pointer, &z, sizeof(z), nullptr);
@@ -289,6 +297,7 @@ int main()
                 inputInteger("How many do you want", amount);
             }   while(amount < 0 || amount > 1000);
 
+            playerInventory.SetPointer(Process, BaseAddress);
             WriteProcessMemory(Process, (LPVOID)(playerInventory.Pointer + 0x38 + (invSlot * 8)), &itemID, sizeof(itemID), nullptr);
             WriteProcessMemory(Process, (LPVOID)(playerInventory.Pointer + 0x3C + (invSlot * 8)), &amount, sizeof(amount), nullptr);
 
@@ -302,12 +311,14 @@ int main()
                 inputInteger("Enter Character ID # (0-2)", charId);
             } while (charId < 0 || charId > 2);
 
+            playerChar.SetPointer(Process, BaseAddress);
             WriteProcessMemory(Process, (LPVOID)playerChar.Pointer, &charId, sizeof(charId), nullptr);
         }
         
         else if (selection == "7"){
             int health;
             inputInteger("input health", health);
+            playerHealth.SetPointer(Process, BaseAddress);
             WriteProcessMemory(Process, (LPVOID)playerHealth.Pointer, &health, sizeof(health), nullptr);
 
         }
